4154e0e91fca1cf500b27d4978281cd5034078a8
madraceee <raajanitheesh@gmail.com>
2026-05-02 14:09:03 +0530

    completed huffman encoding decoding

--- huffman-encoder-decoder/main.go
+++ huffman-encoder-decoder/main.go
@@ -9,10 +9,6 @@ import (
 	"os"
 )
 
-const (
-	READ_SIZE = 1024
-)
-
 type charCount struct {
 	chr   rune
 	count int
@@ -41,6 +37,44 @@ func (pq *pq) Pop() any {
 	return c
 }
 
+type trie struct {
+	chr      rune
+	value    rune
+	isEnd    bool
+	children [2]*trie
+}
+
+func NewTrie() *trie {
+	return &trie{
+		isEnd:    false,
+		children: [2]*trie{},
+	}
+}
+
+func (t *trie) Insert(mask string, value rune) {
+	if len(mask) == 0 {
+		return
+	}
+
+	node := t
+	for _, c := range mask {
+		if node.children[0] == nil || node.children[1] == nil {
+			left := NewTrie()
+			right := NewTrie()
+			node.children = [2]*trie{left, right}
+		}
+
+		if c == '1' {
+			node = node.children[1]
+		} else {
+			node = node.children[0]
+		}
+	}
+
+	node.isEnd = true
+	node.value = value
+}
+
 func main() {
 	// Get file
 	if len(os.Args) < 2 {
@@ -55,14 +89,69 @@ func main() {
 	}
 
 	// DECODE
-	maskByCharD, err := decodeHeader(file)
+	reader1 := bufio.NewReader(file)
+	charByMask, err := decodeHeader(reader1)
 	if err != nil {
 		fmt.Println("ERROR DECODING " + err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Printf("maskByChar: %v\n", maskByCharD)
-	os.Exit(1)
+	searchTree := NewTrie()
+	for k, v := range charByMask {
+		searchTree.Insert(k, v)
+	}
+
+	outFile1, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Printf("Error creating new file " + err.Error())
+		os.Exit(1)
+	}
+	writer1 := bufio.NewWriter(outFile1)
+
+	node := searchTree
+	for {
+		b, err := reader1.ReadByte()
+		if err != nil {
+			break
+		}
+
+		byteMask := ""
+		for range 8 {
+			if b&1 == 1 {
+				byteMask = "1" + byteMask
+			} else {
+				byteMask = "0" + byteMask
+			}
+			b = b >> 1
+		}
+
+		for _, m := range byteMask {
+			if m == '0' {
+				node = node.children[0]
+			} else {
+				node = node.children[1]
+			}
+
+			if node == nil {
+				os.Exit(5)
+			}
+
+			if node.isEnd {
+				if _, err := writer1.WriteRune(node.value); err != nil {
+					fmt.Printf("Error writing to file " + err.Error())
+					os.Exit(1)
+				}
+
+				node = searchTree
+			}
+		}
+
+	}
+
+	writer1.Flush()
+	outFile1.Close()
+	file.Close()
+	os.Exit(0)
 
 	// Get Frequency
 	charCountMap, err := getCharCount(bufio.NewReader(file))
@@ -70,8 +159,6 @@ func main() {
 		fmt.Println("getcharcount " + err.Error())
 		os.Exit(1)
 	}
-
-	charCountMap = map[rune]int{'C': 32, 'D': 42, 'E': 120, 'K': 7, 'L': 42, 'M': 24, 'U': 37, 'Z': 2}
 
 	// Sort
 	charCountArr := make([]*charCount, 0)
@@ -222,13 +309,11 @@ func writeHeader(file *os.File, maskByChar map[rune]string) error {
 	writer := bufio.NewWriter(file)
 
 	for key, mask := range maskByChar {
-		writer.WriteByte(byte(key))
+		writer.WriteRune(key)
 
 		buf := make([]byte, 8)
 		binary.BigEndian.PutUint64(buf, uint64(len(mask)))
 		writer.Write(buf)
-
-		writer.WriteByte(byte('-'))
 
 		var currByte byte
 		var bitCount int
@@ -252,17 +337,16 @@ func writeHeader(file *os.File, maskByChar map[rune]string) error {
 		writer.WriteByte(byte(','))
 	}
 
-	writer.WriteByte(byte('-'))
+	writer.WriteByte(byte('\a'))
+	writer.WriteByte(byte('\a'))
 	writer.Flush()
 	return nil
 }
 
-func decodeHeader(file *os.File) (map[rune]string, error) {
-	reader := bufio.NewReader(file)
-
-	maskByChar := make(map[rune]string)
+func decodeHeader(reader *bufio.Reader) (map[string]rune, error) {
+	charByMask := make(map[string]rune)
 	for {
-		k, err := reader.ReadByte()
+		k, _, err := reader.ReadRune()
 		if err != nil {
 			return nil, err
 		}
@@ -275,11 +359,13 @@ func decodeHeader(file *os.File) (map[rune]string, error) {
 
 		maskLen := binary.BigEndian.Uint64(buf)
 
-		// Read -
-		reader.ReadByte()
-
 		mask := ""
-		noOfBytes := (maskLen + (8 - (maskLen % 8))) / 8
+		noOfBytes := maskLen
+		if maskLen%8 > 0 {
+			noOfBytes = (maskLen + (8 - (maskLen % 8)))
+		}
+		noOfBytes = noOfBytes / 8
+
 		for range noOfBytes {
 			b, err := reader.ReadByte()
 			if err != nil {
@@ -287,10 +373,10 @@ func decodeHeader(file *os.File) (map[rune]string, error) {
 			}
 
 			byteMask := ""
-			if b == 0 {
-				byteMask = "0"
-			}
-			for b > 0 && maskLen > 0 {
+			for range 8 {
+				if maskLen <= 0 {
+					break
+				}
 				if b&1 == 1 {
 					byteMask = "1" + byteMask
 				} else {
@@ -302,22 +388,28 @@ func decodeHeader(file *os.File) (map[rune]string, error) {
 
 			mask = mask + byteMask
 		}
-		maskByChar[rune(k)] = mask
+		charByMask[mask] = k
 
 		token, err := reader.ReadByte()
 		if err != nil {
 			return nil, err
 		}
 
-		nextToken, err := reader.Peek(1)
+		if token != ',' {
+			return nil, fmt.Errorf("MALFORMED HEADER")
+		}
+
+		nextToken, err := reader.Peek(2)
 		if err != nil {
 			return nil, err
 		}
 
-		if token == ',' && nextToken[0] == '-' {
+		if token == ',' && nextToken[0] == '\a' && nextToken[1] == '\a' {
+			reader.ReadByte()
+			reader.ReadByte()
 			break
 		}
 	}
 
-	return maskByChar, nil
+	return charByMask, nil
 }
